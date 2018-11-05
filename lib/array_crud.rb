723700fec2d1186fd75169aa9bfd2acb8299be4c
Samuel Prada <jspradac@gmail.com>
2018-11-05 19:20:39 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,29 @@
 def create_an_empty_array
-  
+  my_array = []
 end
 
 def create_an_array
-  
+  my_array = [2, 3]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array = array 
+  array >> element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_array = array
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  my_array = array
+  my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  my_array = array
+  my_array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
